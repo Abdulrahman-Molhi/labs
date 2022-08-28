51314acfedfbe6cd78f4598f7c1659b09dcf77b8
Abdulrahman-Molhi <112280055+Abdulrahman-Molhi@users.noreply.github.com>
2022-08-28 22:31:34 +0300

    Add files via upload

---
+++ HomeWork5/src/Exercise5.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        String name= JOptionPane.showInputDialog("what is your name?\n");
+        System.out.println("Hello,"+name+"!");
+
+    }
+
+
+
+
+}
